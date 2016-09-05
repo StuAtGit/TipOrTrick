43a89aa6e186e9b34a012d97ef5c0485bf6016f6
StuAtGit <stu26code@gmail.com>
2016-09-05 15:19:31 -0700

    Number keyboard, Change name, Lucky waitress day!

--- app/src/main/java/com/shareplaylearn/androidlearn/MainActivity.java
+++ app/src/main/java/com/shareplaylearn/androidlearn/MainActivity.java
@@ -5,11 +5,49 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Random random = new Random();
+
+    private class TotalPaidListener
+            implements TextView.OnEditorActionListener {
+        TextView totalPaidView;
+        TotalPaidListener(TextView totalPaidView) {
+            this.totalPaidView = totalPaidView;
+        }
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            String totalPaidString = totalPaidView.getText().toString().trim();
+            TextView statusMessage = (TextView) findViewById(R.id.statusMessage);
+            try {
+                double totalPaid = Double.parseDouble(totalPaidString);
+                double tipPercentage = 0.20;
+                if( random.nextInt(5) > 3 ) {
+                    tipPercentage *= 2;
+                    statusMessage.setText("Lucky Waitress Day!!!");
+                } else {
+                    statusMessage.setText("");
+                }
+
+                double tipOwed = totalPaid * tipPercentage;
+                TextView tipOwedView = (TextView) findViewById(R.id.tipOwed);
+                tipOwedView.setText( String.format( "%.2f", tipOwed ) );
+            } catch (NumberFormatException nfe) {
+                statusMessage.setText("Did not enter the total as a number.");
+                return false;
+            }
+            return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +56,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        EditText totalPaidView = (EditText)findViewById(R.id.totalPaid);
+        totalPaidView.setOnEditorActionListener(new TotalPaidListener(totalPaidView));
     }
 
     @Override
